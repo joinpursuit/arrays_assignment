75e38747bad33ac6229841019bbd45550b6b7bad
KathyPuma <kathypuma96@gmail.com>
2019-07-16 12:02:51 -0400

    arrays_lab

--- arrays_lab.js
+++ arrays_lab.js
@@ -17,9 +17,8 @@
 // 1. Log the last element of an array.
 
 let animals = ["seal", "otter", "bear"]
-animals.push("tiger")
-console.log(animals)
-
+console.log(animals[animals.length-1])
+//
 
 // 2. Change the first element of an array to the string "something else".
 
@@ -126,18 +125,18 @@ for(let i = 0; i < logMeOutFor.length ; i++ ){
 console.log(logMeOutFor[i])
 }
 
-
 //
-// 12. Write code that logs every value in an array using a while loop
-//
-// ```js
-// let logMeOutWhile = ["I", "am", "a", "happy", "array"]
-//
-// /* I
-//   am
-//   a
-//   happy
-//   array
+// //
+// // 12. Write code that logs every value in an array using a while loop
+// //
+// // ```js
+// // let logMeOutWhile = ["I", "am", "a", "happy", "array"]
+// //
+// // /* I
+// //   am
+// //   a
+// //   happy
+// //   array
 // */
 // ```
 
@@ -149,58 +148,58 @@ console.log(logMeOutFor[i])
 i++
 }
 
-
 //
-// 13. Write code that logs every value in an array using a for/of loop
+// //
+// // 13. Write code that logs every value in an array using a for/of loop
+// //
+// // ```js
+// // let logMeOutForOf = ["I", "am", "a", "happy", "array"]
+// //
+// // /* I
+// //   am
+// //   a
+// //   happy
+// //   array
+// // */
+// // ```
 //
-// ```js
-// let logMeOutForOf = ["I", "am", "a", "happy", "array"]
-//
-// /* I
-//   am
-//   a
-//   happy
-//   array
-// */
-// ```
-
 let logMeOutFor = ["I", "am", "a", "happy", "array"]
 for(let logMeOut of logMeOutFor){
   console.log(logMeOut)
-}
-
+// }
+//
+// //
+// //
+// // 14. Given an array of numbers, write code that logs the sum of all the numbers
+// //
+// // ```js
+// // let firstArrToSum = [1, 2, 3, 4, 5, 6]
+// // //  21
+// //
+// // let secondArrToSum = [1, 2, 3, 4, 5, -6]
+// // //  9
+// // ```
+// //
 //
 //
-// 14. Given an array of numbers, write code that logs the sum of all the numbers
-//
-// ```js
-// let firstArrToSum = [1, 2, 3, 4, 5, 6]
-// //  21
-//
-// let secondArrToSum = [1, 2, 3, 4, 5, -6]
-// //  9
-// ```
-//
-
-
 let secondArrToSum = [1, 2, 3, 4, 5, -6]
 let sum = 0;
 for(i of secondArrToSum){
   sum += i;
 }
 console.log(sum)
-
-
-
-
-// 15. Given an array of numbers, write code that logs all the odd numbers
 //
-// ```js
 //
-// let evenAndOddArr = [1,5,2,4,11,12,99,100]
 //
-// // 1, 5, 11, 99
-// ```
+//
+// // 15. Given an array of numbers, write code that logs all the odd numbers
+// //
+// // ```js
+// //
+// // let evenAndOddArr = [1,5,2,4,11,12,99,100]
+// //
+// // // 1, 5, 11, 99
+// // ```
 //
 let evenAndOddArr = [1,5,2,4,11,12,99,100]
 for(odd of evenAndOddArr){
@@ -208,20 +207,20 @@ for(odd of evenAndOddArr){
 console.log(`${odd}`)
 }
 }
-
-
-// 16. Given an array of numbers, write code that logs the sum of all the even numbers
 //
-// ```js
 //
-// let evenAndOddArrToSum = [1,5,2,3,11,4,6]
+// // 16. Given an array of numbers, write code that logs the sum of all the even numbers
+// //
+// // ```js
+// //
+// // let evenAndOddArrToSum = [1,5,2,3,11,4,6]
+// //
+// // // 12
+// // ```
+// //
 //
-// // 12
-// ```
 //
-
-
-
+//
 let evenAndOddArrToSum = [1,5,2,3,11,4,6]
 let sumOfEven = 0;
 for(i of evenAndOddArrToSum){
@@ -231,46 +230,56 @@ for(i of evenAndOddArrToSum){
 }
 console.log(sumOfEven)
 
-
-
-
-// 17. Write code such that noDupeList has all the same numbers as dupeFriendlyList, but has no more than one of each number.
 //
-// ```js
-// let dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1]
-// let noDupeList = []
 //
-// // noDupleList = [4, 2, 6, 9, 1]
 //
-// ```
-//
-
-//Note: I searched on google on how to solve and I understand some of it.
-// I have an idea on what to do but I am not sure how to fully execute my ideas.
-
+// // 17. Write code such that noDupeList has all the same numbers as dupeFriendlyList, but has no more than one of each number.
+// //
+// // ```js
+// // let dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1]
+// // let noDupeList = []
+// //
+// // // noDupleList = [4, 2, 6, 9, 1]
+// //
+// // ```
+// //
 
 let dupeFriendlyList = [4,2,6,2,2,6,4,9,2,1];
 let noDupeList = [];
 for(let i = 0; i < dupeFriendlyList.length; i++){
   if(noDupeList.indexOf(dupeFriendlyList[i]) === -1){
     noDupeList.push(dupeFriendlyList[i]);
-      console.log(noDupeList);
+
   }
   }
+console.log(noDupeList);
 
 
 
-
+//
 // 18. Make an array that contains all elements that appear exactly once in someRepeatsAgain
 //
 // ```js
 // let someRepeatsAgain = [25,11,30,31,50,28,4,37,13,20,24,38,28,14,44,33,7,43,39,35,36,42,1,40,7,14,23,46,21,39,11,42,12,38,41,48,20,23,29,24,50,41,38,23,11,30,50,13,13,16,10,8,3,43,10,20,28,39,24,36,21,13,40,25,37,39,31,4,46,20,38,2,7,11,11,41,45,9,49,31,38,23,41,16,49,29,14,6,6,11,5,39,13,17,43,1,1,15,25]
-//
+
 // // [ 44,33,35,42,12,48,30,50,8,3,10,28,24,36,21,1000,40,37,4,46,20,2,7,45,9,31,38,23,41,16,49,29,14,6,11,5,39,13,17,43,1,15,25 ]
 //
 //
 // ```
+
+
 //
+let someRepeatsAgain = [25,11,30,31,50,28,4,37,13,20,24,38,28,14,44,33,7,43,39,35,36,42,1,40,7,14,23,46,21,39,11,42,12,38,41,48,20,23,29,24,50,41,38,23,11,30,50,13,13,16,10,8,3,43,10,20,28,39,24,36,21,13,40,25,37,39,31,4,46,20,38,2,7,11,11,41,45,9,49,31,38,23,41,16,49,29,14,6,6,11,5,39,13,17,43,1,1,15,25]
+let noRepeats = [];
+
+for(let i = 0; i < someRepeatsAgain.length; i++){
+if(someRepeatsAgain.indexOf(someRepeatsAgain[i]) === someRepeatsAgain.lastIndexOf(someRepeatsAgain[i])){
+noRepeats.push(someRepeatsAgain[i]);
+
+}
+}
+console.log(noRepeats)
+
 // 19. Given an array of strings, find the the string with the most "a"s in it.
 //
 // ```js
