aa42c23eb96047ee436d3088568aa0b9e246b88a
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2019-07-15 21:56:05 -0400

    in the arrays lab folder, I added functions to the first 9 questions of the lab.

---
+++ chuck_OkonkwoAguolu.js
@@ -0,0 +1,7 @@
+let person = {"firstName": "Chuck", "lastName": "Okonkwo-Aguolu", "dateOfBirth": "02/24/1994": "bio": "I am Chuck, and Chuck is me"}; 
+
+let random = {"apple": 94, "banana": 16}
+
+console.log(random.kiwi);
+
+console.log(person[0]);
