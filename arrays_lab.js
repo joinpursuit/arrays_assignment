e212aa5741e757be562671f360e546f8b10f5437
Christian01000001 <chris.anselm@gmail.com>
2019-07-22 15:49:00 -0400

    almost done

--- arrays_lab.js
+++ arrays_lab.js
@@ -72,6 +72,68 @@ let logMeOutFor = ["I", "am", "a", "happy", "array"]
 
 for (let i = 0; i < logMeOutFor.length; i++) {
     console.log(logMeOutFor[i]);
-    ;
+}
+
+// 12. Write code that logs every value in an array using a while loop
+
+let logMeOutWhile = ["I", "am", "a", "happy", "array"]
+let i = 0
+while (i < logMeOutWhile.length) {
+    console.log(logMeOutWhile[i]);
+    i++
+}
+
+// 13. Write code that logs every value in an array using a for/of loop
+
+let logMeOutForOf = ["I", "am", "a", "happy", "array"]
+
+for (const value of logMeOutForOf) {
+    console.log(value);
+}
+
+// 14. Given an array of numbers, write code that logs the sum of all the numbers
+
+
+let firstArr = [1, 2, 3, 4, 5, 6]
+
+
+let secondArr = [1, 2, 3, 4, 5, -6]
+//  9
+
+function sumAll(arr){
+    let output = 0
+    for (let i = 0; i < arr.length; i++) {
+        output += arr[i];   
+    }
+    return output
+}
+
+console.log(sumAll(secondArr));
+
+// 15. Given an array of numbers, write code that logs all the odd numbers 
+
+let evenAndOddArr = [1,5,2,4,11,12,99,100]
+// 1, 5, 11, 99
+let answer = []
+
+for (let i = 0; i < evenAndOddArr.length; i++) {
+   if (evenAndOddArr[i] % 2 !== 0) {
+       answer.push(evenAndOddArr[i])
+   }
     
 }
+console.log(answer);
+
+
+// 16. Given an array of numbers, write code that logs the sum of all the even numbers
+
+let evenAndOddArrToSum = [1,5,2,3,11,4,6]
+// 12
+let sum = 0
+
+for (let i = 0; i < evenAndOddArrToSum.length; i++) {
+    if(evenAndOddArrToSum[i] % 2 === 0) {
+        sum += evenAndOddArrToSum[i]
+    }
+}
+console.log(sum);
