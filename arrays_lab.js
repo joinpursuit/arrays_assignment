9cc8d4d37052a422d960c753617deab33bd0757c
trilltown <phil.awich@gmail.com>
2019-10-06 13:33:52 -0400

    Completed problems 1 and 2

---
+++ arrays_lab.js
@@ -0,0 +1,68 @@
+// Problem 1
+
+let fruit = ["strawberry", "banana"];
+
+// a. Add "mango" to the end of a fruits array ["strawberry", "banana"].
+fruit.push("mango")
+// b. Add "blueberry" to the front of the same fruits array.
+fruit.unshift("blueberry")
+// c. Remove the last element of the fruits array.
+fruit.pop()
+// d. console.log the length of the fruits array.
+console.log(fruit.length)
+// e. Remove the first element of the fruits array.
+fruit.shift()
+// f. Join all the elements in the fruits array with a '$'.
+console.log(fruit.join("$"))
+
+// ## 2. Problem Two - Larger array
+
+// Given two arrays, write code that logs the larger array.  If the arrays are the same length, log "They are the same size"
+
+// ```js
+// let firstArr = [1,2,3]
+// let secondArr = [1,5,2,4]
+
+// // log [1,5,2,4]
+// ```
+
+let firstArr = [1,2,3]
+let secondArr = [1,5,2,4]
+
+if (firstArr.length === secondArr.length) {
+    console.log("They are the same size")
+} else if (firstArr.length < secondArr.length) {
+            console.log(secondArr + " secondArr is the larger array")
+        } else {
+            console.log("They are not the same size") 
+        }   
+
+//  ## 3. Problem Three - Middle element
+
+//         Given an array, log its middle element.  If the middle of the array is between two elements, log both of them.
+        
+//         ```js
+//         let hasExactMiddleArr = [1,5,3]
+        
+//         // logs 5
+        
+//         let hasNoExactMiddleArr = [1,4,6,9]
+        
+//         // logs 4,6
+//          ```        
+
+
+let hasExactMiddleArr = [1,5, 3, 5, 6]
+let hasNoExactMiddleArr = [1, 4, 6, 9]
+
+
+// if (hasNoExactMiddleArr.length % 2 === 0) {
+//     console.log()
+// }
+console.log([`${hasExactMiddleArr.length - 1 / 2}`])
+// } else if (firstArr.length < secondArr.length) {
+//             console.log(secondArr + " secondArr is the larger array")
+//         } else {
+//             console.log("They are not the same size") 
+//         }   
+
