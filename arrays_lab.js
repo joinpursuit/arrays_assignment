e4fe898e45f3e797c91775caaf1c0597c87eeb11
kongnyc <54258089+kongnyc@users.noreply.github.com>
2019-10-06 14:06:26 -0400

    Finshed Problem 1-5

--- arrays_lab.js
+++ arrays_lab.js
@@ -27,12 +27,10 @@ if(secondArr.length > firstArr.length){
 
 console.log("prblem 3")
 
-let hasExactMiddleArr = [1,5,3,1,6,1,5,7]
-// let hasNoExactMiddleArr = [1,4,6,9]
+let hasExactMiddleArr = [1,5,3,4,6]
 
-// console.log(hasExactMiddleArr.slice(2,4))
 let middle = Math.floor(hasExactMiddleArr.length/2)
-console.log(middle)
+console.log(middle) //showing the middile #
 
 if (hasExactMiddleArr.length%2===0){
     console.log(hasExactMiddleArr.slice(middle-1, middle+1))
@@ -41,12 +39,31 @@ if (hasExactMiddleArr.length%2===0){
         console.log(hasExactMiddleArr[middle])
 }
 
+console.log("problem 4")
+let arrP4 =["I", "am", "a", "happy", "array"]
+for (let x = 0; x < arrP4.length; x++){
+    console.log(arrP4[x])
+}
+
+console.log("problem 4-b")
+let i = 0
+while (i < arrP4.length){
+    console.log(arrP4[i]);
+    i++;
+}
+
+console.log("problem 4-c")
+const array1 = ['a', 'b', 'c'];
+
+for (element of array1) {
+  console.log(element);
+}
 
 
-// console.log(
-//     hasExactMiddleArr(Math.floor(hasExactMiddleArr.length/2)-1),
-//      (Math.ceil(hasExactMiddleArr.length/2))
-//     )
-// console.log(
-//     hasExactMiddleArr[Math.ceil(hasExactMiddleArr.length/2)-1]
-//     )
+console.log("problem 5")
+
+let firstArrToSum = [1, 2, 3, 4, 5, 6]
+let sum = 0
+for (let x = 0; x < firstArrToSum.length; x++){
+    console.log(sum += firstArrToSum[x])
+}
