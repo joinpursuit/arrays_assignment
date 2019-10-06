6d77d407daea0e8a6c2b58edfa9609170fcf6fe9
Nilber Remon <nilberremon@gmail.com>
2019-10-06 12:26:49 -0400

    Problem 1 Complete

--- arrays_lab.js
+++ arrays_lab.js
@@ -1,17 +1,30 @@
 // 1. Problem One - Fruits array
+
 // a. Add "mango" to the end of a fruits array ["strawberry", "banana"].
 fruits = ["strawberry", "banana"]
 fruits.push("mango")
 console.log(fruits)
 // b. Add "blueberry" to the front of the same fruits array.
 
+fruits.unshift("blueberry")
+console.log(fruits)
+
 // c. Remove the last element of the fruits array.
 
+fruits.pop()
+console.log(fruits)
+
 // d. console.log the length of the fruits array.
+console.log(fruits.length)
 
 // e. Remove the first element of the fruits array.
 
+fruits.shift()
+console.log(fruits)
+
 // f. Join all the elements in the fruits array with a '$'.
+
+console.log(fruits.join("$"))
 
 // 2. Problem Two - Larger array
 // Given two arrays, write code that logs the larger array. If the arrays are the same length, log "They are the same size"
