d4df9fa8970fad3f5c56e8e757a524f8fb86797f
Jay Fowler <jcafowler@gmail.com>
2019-10-06 12:37:51 -0400

    Complete Problem Two

--- arrays_lab.js
+++ arrays_lab.js
@@ -14,10 +14,23 @@
 
 // f. Join all the elements in the fruits array with a '$'.
 
-let fruits = ["strawberry", "banana"];
-fruits.push("mango");
-fruits.unshift("blueberry");
-fruits.pop();
-console.log(fruits.length);
-fruits.shift();
-console.log(fruits.join("$"));
+// let fruits = ["strawberry", "banana"];
+// fruits.push("mango");
+// fruits.unshift("blueberry");
+// fruits.pop();
+// console.log(fruits.length);
+// fruits.shift();
+// console.log(fruits.join("$"));
+
+// ## 2. Problem Two - Larger array
+
+// Given two arrays, write code that logs the larger array.  If the arrays are the same length, log "They are the same size"
+
+let firstArr = [1,2,3,5,]
+let secondArr = [1,5,2,4]
+
+if (firstArr.length > secondArr.length){
+    console.log( firstArr)
+}else if( secondArr.length > firstArr.length){
+    console.log (secondArr)
+}else console.log ("They are the same size")
