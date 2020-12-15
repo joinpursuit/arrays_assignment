2e4d1ea72ea90ffedcf54f29cc77158bbe7dd248
Jasleen Villamil <jasleenvillamil@gmail.com>
2020-12-15 16:20:17 -0500

    Initial commit

--- problems/arraysIntro.js
+++ problems/arraysIntro.js
@@ -4,7 +4,10 @@
  * @param {string} fruit
  * @returns {string[]}
  */
-function addFruitToEnd() {}
+function addFruitToEnd(fruits, fruit) {
+  fruits.push(fruit);
+  return fruits;
+}
 
 /**
  * Adds a fruit to the front of an array of fruits and returns the array.
@@ -12,49 +15,67 @@ function addFruitToEnd() {}
  * @param {string} fruit
  * @returns {string[]}
  */
-function addFruitToFront() {}
+function addFruitToFront(fruits, fruit) {
+  fruits.unshift(fruit);
+  return fruits;
+}
 
 /**
  * Removes the last fruit from an array of fruits and returns the array.
  * @param {string[]} fruits
  * @returns {string[]}
  */
-function removeLast() {}
+function removeLast(fruits) {
+  let removeLast = fruits.pop();
+  return fruits;
+}
 
 /**
  * Removes and returns the last fruit from from an array of fruits
  * @param {string[]} fruits
  * @returns {string} The fruit that was removed
  */
-function removeLastandReturnLast() {}
+function removeLastandReturnLast(fruits) {
+  let removeLast = fruits.pop();
+  return removeLast;
+}
 
 /**
  * Takes in an array and returns the length
  * @param {Array} array
  * @returns {number}
  */
-function arrayLength() {}
+function arrayLength(array) {
+  return array.length;
+}
 
 /**
  * Removes the first fruit in an array of fruits and returns the array.
  * @param {string[]} fruits
  * @returns {string[]}
  */
-function removeFirst() {}
-
+function removeFirst(fruits) {
+  fruits.shift();
+  return fruits;
+}
 /**
  * Removes and returns the first fruit in an array of fruits.
  * @param {string[]} fruits
  * @returns {string} The fruit that was removed
  */
-function removeFirstandReturnFirst() {}
+function removeFirstandReturnFirst(fruits) {
+  let removeFirst = fruits.shift();
+  return removeFirst;
+}
 
 /**
  * Returns a string made by joining all of the elements in an array of fruits with a '$' character.
  * @param {string[]} fruits
  * @returns {string}
  */
-function arrayToString() {}
+function arrayToString(fruits) {
+  return fruits.join("$");
+}
 
 /**
  * Takes in two arrays. And returns the longer array.
@@ -64,7 +85,16 @@ function arrayToString() {}
  * @param {Array} array2
  * @returns {string|Array}
  */
-function largerArray() {}
+//If the 2 are equal return "They are the same size", if otherwise return the greater array
+function largerArray(array1, array2) {
+  if (array1.length === array2.length) {
+    return "They are the same size";
+  } else if (array1.length > array2.length) {
+    return array1;
+  } else {
+    return array2;
+  }
+}
 
 /**
  * Takes in an array and returns the middle element.
@@ -72,7 +102,15 @@ function largerArray() {}
  * @param {Array} array
  * @returns {*|Array}
  */
-function middleElement() {}
+//
+function middleElement(array) {
+  array.length/2
+  
+  
+
+}
+middleElement([1,2,3,4,5]) 
+
 
 /**
  * Takes in an array and returns the last element without altering or mutating the array
